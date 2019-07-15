7e98d5842bbd8c289279ced3907b733ad9a04b43
IVT2 <52491967+IVT2@users.noreply.github.com>
2019-07-16 00:17:22 +0300

    Add files via upload

--- New_ServerApp/ServerApp/Program.cs
+++ New_ServerApp/ServerApp/Program.cs
@@ -10,48 +10,33 @@ namespace ConsoleServer
     
     class Program
     {
-
-        class Person
-        {
-            public string login { get; set; }
-            public string password { get; set; }
-        }
-
         const int port = 8888;
         static TcpListener listener;
         static void Main(string[] args)
-        {
-
-            Dictionary<int, Person> people = new Dictionary<int, Person>();
-            people.Add(1, new Person() { login = "Bill" , password = "1234"});
-            people.Add(2, new Person() { login = "Bill2", password = "5678" });
-            people.Add(3, new Person() { login = "Bill3", password = "4321" });
-
-            foreach (KeyValuePair<int, Person> keyValue in people)
-            {
-                Console.WriteLine(keyValue.Value.login + " - " + keyValue.Value.password);
-                if (keyValue.Value.login == "Bill2" )
-                {
-                    Console.WriteLine("Value : India is present.");
-                }
-                    
-            }
+        {           
 
             try
             {
                 listener = new TcpListener(IPAddress.Parse("192.168.0.115"), port);
                 listener.Start();
+
+                
+
                 Console.WriteLine("Ожидание подключений...");
+
+                
 
                 while (true)
                 {
                     TcpClient client = listener.AcceptTcpClient();
                     ClientObject clientObject = new ClientObject(client);
-
                     // создаем новый поток для обслуживания нового клиента
                     Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
                     clientThread.Start();
                 }
+                
+
+                
             }
             catch (Exception ex)
             {
@@ -64,6 +49,13 @@ namespace ConsoleServer
             }
         }
     }
+
+    class Person
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+
     public class ClientObject
     {
         public TcpClient client;
@@ -78,39 +70,55 @@ namespace ConsoleServer
             try
             {
                 stream = client.GetStream();
-                byte[] data = new byte[64]; // буфер для получаемых данных
+                byte[] passwordData = new byte[64];
                 byte[] userData = new byte[64];
-                while (true)
+                byte[] data = new byte[64];
+
+                // получаем сообщение
+                StringBuilder builder = new StringBuilder();
+                StringBuilder source = new StringBuilder();
+                int passBytes = 0;
+                int UserBytes = 0;
+                do
                 {
-                    // получаем сообщение
-                    StringBuilder builder = new StringBuilder();
-                    StringBuilder source = new StringBuilder();
-                    int bytes = 0;
-                    int UserBytes = 0;
-                    do
-                    {
-                        bytes = stream.Read(data, 0, data.Length);
-                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-                        UserBytes = stream.Read(userData, 0, userData.Length);
-                        source.Append(Encoding.Unicode.GetString(userData, 0, UserBytes));
-                    }
-                    while (stream.DataAvailable);
+                    UserBytes = stream.Read(userData, 0, userData.Length);
+                    source.Append(Encoding.Unicode.GetString(userData, 0, UserBytes));
+                    passBytes = stream.Read(passwordData, 0, passwordData.Length);
+                    builder.Append(Encoding.Unicode.GetString(passwordData, 0, passBytes));
+                    
+                }
+                while (stream.DataAvailable); 
 
                     
 
-                    string message = builder.ToString();
-                    string src = source.ToString();
+                string pass = builder.ToString();
+                string login = source.ToString();
 
-                    Console.WriteLine(src);
+                string allertmasseg = "User dosnt exist";
 
-                    Console.WriteLine(message);
+                List<Person> people = new List<Person>();
+                people.Add(new Person() { login = "ivt22222", password = "ivt2121" });
 
-
-                    // отправляем обратно сообщение в верхнем регистре
-                    message = message.Substring(message.IndexOf(':') + 1).Trim().ToUpper();
-                    data = Encoding.Unicode.GetBytes(message);
-                    stream.Write(data, 0, data.Length);
+                foreach (Person p in people)
+                {
+                    if (p.login == login & p.password == pass)
+                    {
+                        Console.WriteLine("Ok...");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not Found");
+                        data = Encoding.Unicode.GetBytes(allertmasseg);
+                        stream.Write(data, 0, data.Length);
+                    }
                 }
+
+                Console.WriteLine(login);
+
+                Console.WriteLine(pass);
+              
+                
             }
             catch (Exception ex)
             {
